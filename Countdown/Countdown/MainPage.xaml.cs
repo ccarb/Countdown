c9f1b36755d05b71668e0ed1ec0c3c3aaf93ba7d
Developer <Developer@NBASUS-CECI>
2022-12-12 19:52:19 -0300

    Agregar archivos de proyecto.

---
+++ Countdown/Countdown/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Countdown
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int count = 0;
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            count++;
+        }
+
+        void GoToCreditsView(object sender, System.EventArgs e)
+        {
+            count++;
+        }
+        async void AddNewCountdown(object sender, System.EventArgs e)
+        {
+            var NewCountdownPage = new NewCountdownPage();
+            await Navigation.PushModalAsync(NewCountdownPage);
+        }
+    }
+}
