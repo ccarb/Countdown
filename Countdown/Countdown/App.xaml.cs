1bea7106e3382c6190a30992d4a100a20d4f56e9
Cecilia Carbonaro <cecilia.carbonaro@gmail.com>
2022-12-13 14:05:18 -0300

    Working database and countdown cards

--- Countdown/Countdown/App.xaml.cs
+++ Countdown/Countdown/App.xaml.cs
@@ -2,6 +2,8 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using SQLite;
+
 namespace Countdown
 {
     public partial class App : Application
@@ -9,6 +11,23 @@ namespace Countdown
         public App()
         {
             InitializeComponent();
+
+            // Access or create DB
+            var db = new SQLiteConnection(Constants.DatabasePath);
+
+            db.CreateTable<CountdownTable>();
+            // This is for testing only, delete for prod
+            if (db.Table<CountdownTable>().Count() == 0)
+            {
+                // only insert the data if it doesn't already exist
+                var newCountdown = new CountdownTable();
+                newCountdown.Name = "Example";
+                newCountdown.Description = "A hardcoded example for testing purposes";
+                newCountdown.Created = DateTime.Now;
+                newCountdown.Due = DateTime.Now;
+                newCountdown.Due.AddDays(3);
+                db.Insert(newCountdown);
+            }
 
             MainPage = new MainPage();
         }
--- Countdown/Countdown/MainPage.xaml.cs
+++ Countdown/Countdown/MainPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Countdown
 {
@@ -13,6 +15,8 @@ namespace Countdown
         public MainPage()
         {
             InitializeComponent();
+
+            UpdateCountdownList();
         }
         int count = 0;
         void Handle_Clicked(object sender, System.EventArgs e)
@@ -28,6 +32,51 @@ namespace Countdown
         {
             var NewCountdownPage = new NewCountdownPage();
             await Navigation.PushModalAsync(NewCountdownPage);
+            UpdateCountdownList();
+        }
+        void UpdateCountdownList()
+        {
+            
+            // Access or create DB
+            var db = new SQLiteConnection(Constants.DatabasePath);
+            if (db.Table<CountdownTable>().Count() != 0)
+            {
+                CountdownLayout.Children.Clear();
+                var countdownRecords = db.Table<CountdownTable>();
+                foreach (var record in countdownRecords)
+                {
+                    Frame countdownCard = new Frame
+                    {
+                        HeightRequest = 160,
+                        WidthRequest = 300,
+                        HorizontalOptions = LayoutOptions.Center,
+                        BackgroundColor = Color.CornflowerBlue,
+                        Content = new StackLayout
+                        {
+                            Children =
+                        {
+                            new Label
+                            {
+                                Text=record.Name,
+                            },
+                            new Label
+                            {
+                                Text=record.Description,
+                            },
+                            new Label
+                            {
+                                Text="Due " + record.Due.ToShortDateString() , FontSize=36,
+                            },
+                            new Label
+                            {
+                                Text="Created " + record.Created.ToLongDateString(),
+                            }
+                        }
+                        }
+                    };
+                    CountdownLayout.Children.Add(countdownCard);
+                }
+            }
         }
     }
 }
