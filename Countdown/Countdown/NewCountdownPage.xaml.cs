26a02a2a61357e30307f4f6428100a384458f369
Cecilia Carbonaro <cecilia.carbonaro@gmail.com>
2022-12-13 10:29:46 -0300

    Working new countdown page

---
+++ Countdown/Countdown/NewCountdownPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Countdown
+{
+    public partial class NewCountdownPage : ContentPage
+    {
+        public NewCountdownPage()
+        {
+            InitializeComponent();
+        }
+        async void CreateCountdown(object sender, System.EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
